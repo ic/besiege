c57c1c3725e47fbe476166e258bfe7d21f8153a9
Eric Platon <zaraki@gmx.com>
2014-02-06 18:13:34 +0900

    HAML templating.

--- lib/besiege/app.rb
+++ lib/besiege/app.rb
@@ -1,19 +1,24 @@
 require 'open3'
 
 require 'sinatra/base'
+require 'haml'
 
 module Besiege
   class App < Sinatra::Base
 
+    configure do
+      set :haml, :format => :html5
+    end
+
     get '/*' do
-      response = ["<!DOCTYPE html><html><head></head><body><h1>Report</h1>"]
+      response = []
       Open3.popen3("siege -r10 -b -i -c50 -l/tmp/siege.log http://#{params[:splat].first}") do |sin, sout, serr, thr|
         status = thr.value
         lines = serr.readlines
         response.concat(lines[5..-1].map{|l| l.chomp + '<br />'})
       end
-      response << "</body></html>"
-      response.join
+      @report = response.join
+      haml :report
     end
 
   end
